13a6a5e446539b37fc47e2595c710700962bea2f
EICHAN <ek.h1.hy@gmail.com>
2026-04-10 23:50:26 +0900

    Refactor Max function and tests to handle edge cases and improve test organization using table-driven tests

--- problems/02_max/max.go
+++ problems/02_max/max.go
@@ -1,8 +1,11 @@
 package max
 
 func Max(nums []int) int {
-	max := 0
-	for _, num := range nums {
+	if len(nums) == 0 {
+		return 0
+	}
+	max := nums[0]
+	for _, num := range nums[1:] {
 		if num > max {
 			max = num
 		}
--- problems/02_max/max_test.go
+++ problems/02_max/max_test.go
@@ -5,9 +5,22 @@ import (
 )
 
 func TestMax(t *testing.T) {
-	max := Max([]int{4, 9, 2, 7})
-	want := 9
-	if max != want {
-		t.Errorf("got %d, want %d", max, want)
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{name: "normal case", nums: []int{4, 9, 2, 7}, want: 9},
+		{name: "empty case", nums: []int{}, want: 0},
+		{name: "negative case", nums: []int{-4, -9, -2, -7}, want: -2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Max(tt.nums)
+			want := tt.want
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
 	}
 }
